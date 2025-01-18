3aacfef283db1888ea875cc4bb11d876bc3a6568
ucokgeming <lendingatuh@gmail.com>
2025-01-18 16:35:31 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# Inisialisasi aplikasi Flask
+app = Flask(__name__)
+
+# Routing untuk halaman utama
+@app.route('/')
+def home():
+    return "Hello, Flask on Koyeb!"
+
+# Menjalankan aplikasi
+if __name__ == '__main__':
+    # Pastikan aplikasi berjalan di host '0.0.0.0' dan port 5000
+    app.run(host='0.0.0.0', port=5000)
